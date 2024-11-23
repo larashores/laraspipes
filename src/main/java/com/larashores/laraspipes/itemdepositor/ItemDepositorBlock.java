6b1e2e24ce3345d02b7cbca9af1744cee9b2acee
Lara Shores <lara.shores@outlook.com>
2024-11-23 00:47:42 -0800

    ItemDepositorModels updates

--- src/main/java/com/larashores/laraspipes/itemdepositor/ItemDepositorBlock.java
+++ src/main/java/com/larashores/laraspipes/itemdepositor/ItemDepositorBlock.java
@@ -2,6 +2,7 @@ package com.larashores.laraspipes.itemdepositor;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -16,15 +17,26 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
 
 public class ItemDepositorBlock extends Block implements EntityBlock {
     private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final Map<Direction, BooleanProperty> CONNECTED = Map.of(
+        Direction.SOUTH, BooleanProperty.create("south"),
+        Direction.EAST, BooleanProperty.create("east"),
+        Direction.WEST, BooleanProperty.create("west"),
+        Direction.NORTH, BooleanProperty.create("north"),
+        Direction.UP, BooleanProperty.create("up"),
+        Direction.DOWN, BooleanProperty.create("down")
+    );
 
     public ItemDepositorBlock() {
         super(
@@ -68,14 +80,19 @@ public class ItemDepositorBlock extends Block implements EntityBlock {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(BlockStateProperties.FACING);
+        for (var property : CONNECTED.values()) {
+            builder.add(property);
+        }
     }
 
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (
-            defaultBlockState()
-                .setValue(BlockStateProperties.FACING, context.getNearestLookingDirection())
-        );
+        var state = defaultBlockState();
+        state.setValue(BlockStateProperties.FACING, context.getNearestLookingDirection());
+        for (var property : CONNECTED.values()) {
+            state.setValue(property, true);
+        }
+        return state;
     }
 }
--- src/main/java/com/larashores/laraspipes/itemextractor/ItemExtractorBlock.java
+++ src/main/java/com/larashores/laraspipes/itemextractor/ItemExtractorBlock.java
@@ -2,6 +2,7 @@ package com.larashores.laraspipes.itemextractor;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -14,11 +15,23 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
+import java.util.Map;
+
 public class ItemExtractorBlock extends Block implements EntityBlock {
     private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final Map<Direction, BooleanProperty> CONNECTED = Map.of(
+        Direction.SOUTH, BooleanProperty.create("south"),
+        Direction.EAST, BooleanProperty.create("east"),
+        Direction.WEST, BooleanProperty.create("west"),
+        Direction.NORTH, BooleanProperty.create("north"),
+        Direction.UP, BooleanProperty.create("up"),
+        Direction.DOWN, BooleanProperty.create("down")
+    );
 
     public ItemExtractorBlock() {
         super(
@@ -59,14 +72,19 @@ public class ItemExtractorBlock extends Block implements EntityBlock {
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(BlockStateProperties.FACING);
+        for (var property : CONNECTED.values()) {
+            builder.add(property);
+        }
     }
 
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (
-            defaultBlockState()
-            .setValue(BlockStateProperties.FACING, context.getNearestLookingDirection())
-        );
+        var state = defaultBlockState();
+        state.setValue(BlockStateProperties.FACING, context.getNearestLookingDirection());
+        for (var property : CONNECTED.values()) {
+            state.setValue(property, true);
+        }
+        return state;
     }
 }
