f2712fb1491797a264f7c6d525ef6e05fca7662e
Lara Shores <lara.shores@outlook.com>
2024-11-23 00:47:42 -0800

    ItemDepositorModels updates

--- src/main/java/com/larashores/laraspipes/itemextractor/ItemExtractorModels.java
+++ src/main/java/com/larashores/laraspipes/itemextractor/ItemExtractorModels.java
@@ -2,6 +2,7 @@ package com.larashores.laraspipes.itemextractor;
 
 import com.larashores.laraspipes.Main;
 import com.larashores.laraspipes.Registration;
+import com.larashores.laraspipes.itemdepositor.ItemDepositorBlock;
 import com.larashores.laraspipes.utils.Utils;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
@@ -16,17 +17,26 @@ public class ItemExtractorModels {
         MultiPartBlockStateBuilder blockStateBuilder
     ) {
         var path = Registration.EXTRACTOR.getId().getPath();
-        var modelBuilder = models.getBuilder("block/" + path);
-        modelBuilder.parent(models.getExistingFile(new ResourceLocation("cube_all")));
-        modelBuilder.texture("all", Main.MOD_ID + ":block/" + path);
-        modelBuilder.element()
-            .from(5, 5, 0)
-            .to(11, 11, 8)
+        var frontModeBuilder = models.getBuilder("block/" + path + "_front");
+        frontModeBuilder.parent(models.getExistingFile(new ResourceLocation("cube_all")));
+        frontModeBuilder.texture("all", Main.MOD_ID + ":block/" + path);
+        frontModeBuilder.element()
+            .from(5, 5, 5)
+            .to(11, 11, 11)
             .allFaces((direction, faceBuilder) -> faceBuilder.texture("#all"))
             .end();
-        modelBuilder.element()
-            .from(1, 0, 8)
+        frontModeBuilder.element()
+            .from(1, 0, 11)
             .to(15, 14, 17)
+            .allFaces((direction, faceBuilder) -> faceBuilder.texture("#all"))
+            .end();
+
+        var sideBuilder = models.getBuilder("block/" + path + "_side");
+        sideBuilder.parent(models.getExistingFile(new ResourceLocation("cube_all")));
+        sideBuilder.texture("all", Main.MOD_ID + ":block/" + path);
+        sideBuilder.element()
+            .from(5, 5, 11)
+            .to(11, 11, 16)
             .allFaces((direction, faceBuilder) -> faceBuilder.texture("#all"))
             .end();
 
@@ -34,11 +44,19 @@ public class ItemExtractorModels {
             var rotation = Utils.getRotation(direction);
             blockStateBuilder
                 .part()
-                .modelFile(modelBuilder)
+                .modelFile(frontModeBuilder)
                 .rotationX(rotation.x())
                 .rotationY(rotation.y())
                 .addModel()
                 .condition(BlockStateProperties.FACING, direction);
+
+            blockStateBuilder
+                .part()
+                .modelFile(sideBuilder)
+                .rotationX(rotation.x())
+                .rotationY(rotation.y())
+                .addModel()
+                .condition(ItemExtractorBlock.CONNECTED.get(direction), true);
         }
     }
 }
