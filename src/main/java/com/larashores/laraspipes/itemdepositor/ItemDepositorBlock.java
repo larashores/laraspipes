838d7454b7895f0ba9288785aa8eb4fbeec7856e
Lara Shores <lara.shores@outlook.com>
2024-11-22 22:42:43 -0800

    Add ItemDepositorModels

--- src/main/java/com/larashores/laraspipes/itemdepositor/ItemDepositorBlock.java
+++ src/main/java/com/larashores/laraspipes/itemdepositor/ItemDepositorBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -13,6 +14,8 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
@@ -26,6 +29,7 @@ public class ItemDepositorBlock extends Block implements EntityBlock {
     public ItemDepositorBlock() {
         super(
             BlockBehaviour.Properties.of()
+            .noOcclusion()
             .strength(3.5F)
             .sound(SoundType.METAL)
         );
@@ -59,5 +63,19 @@ public class ItemDepositorBlock extends Block implements EntityBlock {
             }
         }
         return InteractionResult.PASS;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(BlockStateProperties.FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return (
+            defaultBlockState()
+                .setValue(BlockStateProperties.FACING, context.getNearestLookingDirection())
+        );
     }
 }
