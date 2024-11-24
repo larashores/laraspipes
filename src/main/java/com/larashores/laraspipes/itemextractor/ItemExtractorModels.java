c5703074d8f8927e6b36d8f20a7fc2da2111471e
Lara Shores <lara.shores@outlook.com>
2024-11-23 22:30:19 -0800

    Set particle textures

--- src/main/java/com/larashores/laraspipes/itemextractor/ItemExtractorModels.java
+++ src/main/java/com/larashores/laraspipes/itemextractor/ItemExtractorModels.java
@@ -2,7 +2,6 @@ package com.larashores.laraspipes.itemextractor;
 
 import com.larashores.laraspipes.Main;
 import com.larashores.laraspipes.Registration;
-import com.larashores.laraspipes.itemdepositor.ItemDepositorBlock;
 import com.larashores.laraspipes.utils.Utils;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
@@ -22,6 +21,7 @@ public class ItemExtractorModels {
         frontModeBuilder.texture("back", Main.MOD_ID + ":block/" + path + "_back");
         frontModeBuilder.texture("front", Main.MOD_ID + ":block/" + path + "_front");
         frontModeBuilder.texture("pipe", Main.MOD_ID + ":block/" + path + "_pipe");
+        frontModeBuilder.texture("particle", Main.MOD_ID + ":block/" + path + "_pipe");
         frontModeBuilder.texture("vertical", Main.MOD_ID + ":block/" + path + "_side_vertical");
         frontModeBuilder.texture("horizontal", Main.MOD_ID + ":block/" + path + "_side_horizontal");
         frontModeBuilder.element()
@@ -58,6 +58,7 @@ public class ItemExtractorModels {
         var sideBuilder = models.getBuilder("block/" + path + "_side");
         sideBuilder.parent(models.getExistingFile(new ResourceLocation("cube_all")));
         sideBuilder.texture("pipe", Main.MOD_ID + ":block/" + path + "_pipe");
+        frontModeBuilder.texture("particle", Main.MOD_ID + ":block/" + path + "_pipe");
         sideBuilder.element()
             .from(5, 5, 11)
             .to(11, 11, 16)
