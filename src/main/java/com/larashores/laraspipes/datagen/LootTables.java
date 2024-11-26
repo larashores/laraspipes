05a31c475f16dd979aa2503dd0c98d0c844cd6be
Lara Shores <lara.shores@outlook.com>
2024-11-25 18:07:10 -0800

    Add generated resources

--- src/main/java/com/larashores/laraspipes/datagen/LootTables.java
+++ src/main/java/com/larashores/laraspipes/datagen/LootTables.java
@@ -1,12 +1,25 @@
 package com.larashores.laraspipes.datagen;
 
 import com.larashores.laraspipes.Registration;
-import net.minecraft.data.loot.packs.VanillaBlockLoot;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
 
-public class LootTables extends VanillaBlockLoot {
+import java.util.Set;
+
+public class LootTables extends BlockLootSubProvider {
+    public LootTables() {
+        super(
+            Set.of(
+                Registration.DEPOSITOR_ITEM.get(),
+                Registration.EXTRACTOR_ITEM.get(),
+                Registration.PIPE_ITEM.get()
+            ),
+            FeatureFlags.REGISTRY.allFlags()
+        );
+    }
+
     @Override
     protected void generate() {
-        super.generate();
         dropSelf(Registration.DEPOSITOR.get());
         dropSelf(Registration.EXTRACTOR.get());
         dropSelf(Registration.PIPE.get());
